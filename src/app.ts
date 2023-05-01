37b9f1e67dbf61b22add7be9cfb7da6539dcce0a
d0422 <rlfehd2013@naver.com>
2023-05-01 17:59:11 +0900

    feat: typeORM setting

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Express, Request, Response } from 'express';
+import startDB from './index';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app: Express = express();
 const port = 8080;
+startDB();
 app.get('/', (req: Request, res: Response) => {
   res.send('Server Setting');
 });
---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from './entity/User';
+import dotenv from 'dotenv';
+dotenv.config();
+const { DB_USER, DB_PASSWORD } = process.env;
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: 'friending_db',
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
