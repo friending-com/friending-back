a9b299681493dec508f9c4c910be84a57641a55c
d0422 <rlfehd2013@naver.com>
2023-05-02 17:15:33 +0900

    feat: profile 오류 처리

--- src/router/profile.ts
+++ src/router/profile.ts
@@ -3,8 +3,19 @@ import getProfile from '../DAO/profile';
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-  const id = req.query.id as unknown as number;
-  const user = await getProfile(id);
-  res.json(user);
+  try {
+    const id = req.query.id as unknown as number;
+    if (!id) {
+      throw new Error('query없음');
+    }
+    const user = await getProfile(id);
+    if (user === null) {
+      res.json('user가 존재하지 않습니다.');
+    } else {
+      res.json(user);
+    }
+  } catch (err) {
+    res.json('오류가 발생했습니다.');
+  }
 });
 export { router as profile };
