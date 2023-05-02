889b6037bf4061a2261c3cd9e4d4b499a92dff23
d0422 <rlfehd2013@naver.com>
2023-05-02 17:14:52 +0900

    feat: DB연결 단일 처리

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import startDB from './DAO/signup';
 import dotenv from 'dotenv';
 import { signup } from './router/signUp';
 import { profile } from './router/profile';
+import { AppDataSource } from './data-source';
 dotenv.config();
 
 const app: Express = express();
 const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+AppDataSource.initialize()
+  .then(async () => {})
+  .catch((err) => console.log(err));
 app.use('/signup', signup);
 app.use('/profile', profile);
 app.get('/', (req: Request, res: Response) => {
