754355bda84fa9c882a4603295907505275cc794
d0422 <rlfehd2013@naver.com>
2023-05-02 16:39:23 +0900

    chore: signup 코드 정리

--- src/DAO/signup.ts
+++ src/DAO/signup.ts
@@ -21,15 +21,6 @@ const signupExecute = (
       user.kakaoTalk = kakaoTalk;
       user.age = age;
       await AppDataSource.manager.save(user);
-      console.log('Saved a new user with id: ' + user.id);
-
-      console.log('Loading users from the database...');
-      const users = await AppDataSource.manager.find(User);
-      console.log('Loaded users: ', users);
-
-      console.log(
-        'Here you can setup and run express / fastify / any other framework.'
-      );
     })
     .catch((error) => console.log(error));
 };
