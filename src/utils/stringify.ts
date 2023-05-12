ead695decd509d72e9e3a5a0c5c2057e16488466
d0422 <rlfehd2013@naver.com>
2023-05-12 15:08:38 +0900

    chore: stringify삭제

--- src/utils/stringify.ts
+++
@@ -1,9 +0,0 @@
-export const addHashString = (originString: string, addNumber: number) => {
-  return `${originString},${addNumber}`;
-};
-export const numberize = (string: string) => {
-  return string.split(',').map((str) => Number(str));
-};
-export const stringify = (array: number[]) => {
-  return array.join(',');
-};
