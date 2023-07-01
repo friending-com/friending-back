69690f99d4898196803d610384b4ae047e88d0da
d0422 <rlfehd2013@naver.com>
2023-07-01 09:46:23 +0900

    feat: profile body추가 및 권한확인작업 추가

--- src/controllers/NavigateController.ts
+++ src/controllers/NavigateController.ts
@@ -3,11 +3,19 @@ import { NavigateService } from '../services/NavigateService';
 import { token } from '../utils/auth';
 import { UserService } from '../services/UserService';
 import ErrorStatus from '../utils/ErrorStatus';
+import { AuthorizationService } from '../services/AuthorizationService';
 
 export class NavigateController {
   static async post(req: Request, res: Response) {
     const findProfile = req.body.findProfile as unknown as number;
-    const userProfile = token(req.headers.authorization);
+    const userProfile = req.body.userProfile as unknown as number;
+    const user = token(req.headers.authorization);
+    const authResult = await AuthorizationService.doesUserHaveProfile(
+      user,
+      userProfile
+    );
+    if (!authResult)
+      throw new ErrorStatus('프로필의 소유 권한이 없습니다!', 400);
     const result = await NavigateService.navigate(userProfile, findProfile);
     if (result) res.json(result);
     else res.json('navigate 결과가 없습니다');
