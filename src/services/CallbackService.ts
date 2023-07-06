1752a7076fcb14985730d265ca533539c0488fd0
d0422 <rlfehd2013@naver.com>
2023-07-06 15:10:22 +0900

    feat: access token요청부 변경

--- src/services/CallbackService.ts
+++ src/services/CallbackService.ts
@@ -1,18 +1,17 @@
-import axios from 'axios';
+import axios, { isAxiosError } from 'axios';
 export class CallbackService {
   static async googlePost(code: string) {
     try {
-      console.log(process.env.GOOGLE_CLIENT_ID);
       const token = await axios.post('https://oauth2.googleapis.com/token', {
         code,
         client_id: process.env.GOOGLE_CLIENT_ID,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET,
         redirect_uri: process.env.GOOGLE_REDIRECT_URI,
         grant_type: 'authorization_code',
       });
-      console.log(token);
+      console.log(token.data);
     } catch (err) {
-      console.log(err);
+      if (isAxiosError(err))
+        console.log(err.response.status, err.response.data);
     }
   }
 }
