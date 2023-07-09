fc2e5bc2da0325d51467edb0df7841fa2bdaf94c
d0422 <rlfehd2013@naver.com>
2023-07-09 22:41:08 +0900

    fix:accessToken으로 변경

--- src/controllers/CallbackController.ts
+++ src/controllers/CallbackController.ts
@@ -3,7 +3,7 @@ import { CallbackService } from '../services/CallbackService';
 import { LoginService } from '../services/LoginService';
 export class CallbackController {
   static async post(req: Request, res: Response) {
-    const accessToken = req.body.code;
+    const { accessToken } = req.body;
     const data = await CallbackService.google(accessToken);
     const tokens = await LoginService.googleLogin(data);
     res.json(tokens);
