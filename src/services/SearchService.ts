0fcdd8961d824912d4abb9026da62484bd3d718d
d0422 <rlfehd2013@naver.com>
2023-07-09 20:58:59 +0900

    feat: User엔티티 변경으로 인한 SearchService변경

--- src/services/SearchService.ts
+++ src/services/SearchService.ts
@@ -5,20 +5,19 @@ export class SearchService {
   static async getSearchResult(searchQuery: string) {
     const hashTagResult = await HashTagDAO.getHashTagAutoMatching(searchQuery);
 
-    const userData = await UserDAO.getUserProfilesByName(searchQuery);
-    const userProfiles = userData
-      ? userData.profiles.filter((profile) => profile.isPublic === true)
-      : [];
-    if (userProfiles.length) {
-      Object.entries(userData).forEach(([key, value]) => {
-        if (key != 'profiles') {
-          userProfiles.map((profile) => (profile[key] = value));
-        }
-      });
-    }
+    // const userData = await UserDAO.getUserProfilesByName(searchQuery);
+    // const userProfiles = userData
+    //   ? userData.profiles.filter((profile) => profile.isPublic === true)
+    //   : [];
+    // if (userProfiles.length) {
+    //   Object.entries(userData).forEach(([key, value]) => {
+    //     if (key != 'profiles') {
+    //       userProfiles.map((profile) => (profile[key] = value));
+    //     }
+    //   });
+    // }
     return {
       hashTag: hashTagResult.map((hashTag) => hashTag.hashTag),
-      profiles: userProfiles,
     };
   }
 }
