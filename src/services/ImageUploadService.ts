bee6021dd3bcf01f8f3a8ea5f3a067e24e475d32
d0422 <rlfehd2013@naver.com>
2023-07-31 00:16:25 +0900

    feat: 이미지 업로드 기능 추가

--- src/services/ImageUploadService.ts
+++ src/services/ImageUploadService.ts
@@ -1,21 +1,32 @@
-import multer_s3 from 'multer-s3';
-import aws from 'aws-sdk';
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-});
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { Request } from 'express';
+import multer from 'multer';
+import dotenv from 'dotenv';
+dotenv.config();
+const s3Config = {
+  region: 'ap-northeast-2',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+};
+const storage = multer.memoryStorage();
+export const multerSetting = multer({ storage: storage });
 export class ImageUploadService {
-  storage = multer_s3({
-    s3: s3,
-    bucket: '-', // 자신의 s3 버킷 이름
-    contentType: multer_s3.AUTO_CONTENT_TYPE,
-    acl: 'public-read', // 버킷에서 acl 관련 설정을 풀어줘야 사용할 수 있다.
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      cb(null, `contents/${Date.now()}_${file.originalname}`);
-    },
-  });
-  static upload() {}
+  static client = new S3Client(s3Config);
+  static async upload(req: Request) {
+    const file = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: req.file.originalname,
+      Body: req.file.buffer,
+    };
+    try {
+      const response = await ImageUploadService.client.send(
+        new PutObjectCommand(file)
+      );
+      return response;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
