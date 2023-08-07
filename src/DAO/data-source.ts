7d6d1c0925e678a8f2f0d0b2015e35c41df24100
d0422 <rlfehd2013@naver.com>
2023-08-07 15:53:32 +0900

    test: 테스트 환경 구성

--- src/DAO/data-source.ts
+++ src/DAO/data-source.ts
@@ -5,14 +5,19 @@ import dotenv from 'dotenv';
 import { HashTag } from '../entity/HashTag';
 import { Profile } from '../entity/Profile';
 dotenv.config();
-const { DB_USER, DB_PASSWORD } = process.env;
+
+const { NODE_ENV, DB_USER, DB_PASSWORD } = process.env;
+const isTestEnvironment = NODE_ENV === 'test';
+const port = isTestEnvironment ? 3307 : 3306;
+const db = isTestEnvironment ? 'friending_test_db' : 'friending_db';
+
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
-  port: 3306,
+  port: port,
   username: DB_USER,
   password: DB_PASSWORD,
-  database: 'friending_db',
+  database: db,
   synchronize: true,
   logging: false,
   entities: [User, HashTag, Profile],
