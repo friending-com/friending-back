01cf4d0fbc01390153eb2526ff16e2411b11c55e
d0422 <rlfehd2013@naver.com>
2023-08-07 18:33:06 +0900

    test: mysql initialize 대기하는 코드 추가

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+source ./.env
 docker-compose up -d
 
 sh ./scripts/wait-for-it.sh localhost:3307 -t 15
-echo -e "(●ˇ∀ˇ●) 도커 세팅중..."
-sleep 20
+
+until docker container exec -it mySQLContainer mysqladmin ping -P 3306 -u root -p$DB_PASSWORD | grep "mysqld is alive" ; do
+  >&2 echo "MySQL is unavailable - waiting for it... "
+  sleep 1
+done
 
 jest --setupFiles dotenv/config
 
