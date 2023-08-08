92ed77fba6ffd766f404291902a8e518ec3de37e
d0422 <rlfehd2013@naver.com>
2023-08-08 12:04:49 +0900

    test: 테스트코드 리팩터링 (describe it패턴)

--- tests/app.ts
+++ tests/app.ts
@@ -9,7 +9,6 @@ beforeAll(async () => {
   dotenv.config();
   try {
     await AppDataSource.initialize();
-    console.log('연결!');
   } catch (err) {
     console.log(err);
   }
@@ -18,18 +17,27 @@ afterAll(async () => {
   await AppDataSource.destroy();
 });
 
-test('Login Test', async () => {
-  const result = await LoginService.login({
-    email: 'rlfehd2013@naver.com',
-    name: 'gildong',
-    picture: '1',
+describe('Login Test', () => {
+  it('access and refresh', async () => {
+    const result = await LoginService.login({
+      email: 'rlfehd2013@naver.com',
+      name: 'gildong',
+      picture: '1',
+    });
+    expect(result.access).toBeTruthy();
+    expect(result.refresh).toBeTruthy();
   });
-  expect(result.access).toBeTruthy();
-  expect(result.refresh).toBeTruthy();
 });
 
-test('Profile Create Test', async () => {
-  const profileData = {
+describe('Profile Create Test', () => {
+  const noWorkSpace = {
+    name: '길동',
+    isPublic: true,
+    hashTags: ['중앙대학교'],
+    email: 'rlfehd2013@naver.com',
+    userId: 1,
+  };
+  const normalData = {
     name: '길동',
     isPublic: true,
     hashTags: ['중앙대학교'],
@@ -37,17 +45,34 @@ test('Profile Create Test', async () => {
     userId: 1,
     workSpace: '프렌딩',
   };
-  const result = await ProfileService.createProfile(
-    profileData as ProfileCreateData
-  );
-  expect(result.hashTags[0].hashTag).toBe('중앙대학교');
-  expect(result.workSpace.hashTag).toBe('프렌딩');
-  expect(result.email).toBe('rlfehd2013@naver.com');
-});
 
-test('GetALL Profile Test', async () => {
-  const result = await UserService.findAllProfile(1);
-  expect(result[0].hashTags[0].hashTag).toBe('중앙대학교');
-  expect(result[0].workSpace.hashTag).toBe('프렌딩');
-  expect(result[0].email).toBe('rlfehd2013@naver.com');
+  it('normal Profile', async () => {
+    const result = await ProfileService.createProfile(
+      normalData as ProfileCreateData
+    );
+    expect(result.hashTags[0].hashTag).toBe('중앙대학교');
+    expect(result.workSpace.hashTag).toBe('프렌딩');
+    expect(result.email).toBe('rlfehd2013@naver.com');
+    await ProfileService.deleteProfile(result.id);
+  });
+
+  it('not workSpace', async () => {
+    const result = await ProfileService.createProfile(
+      noWorkSpace as ProfileCreateData
+    );
+    expect(result.hashTags[0].hashTag).toBe('중앙대학교');
+    expect(result.email).toBe('rlfehd2013@naver.com');
+    await ProfileService.deleteProfile(result.id);
+  });
+
+  it('GetALL Profile Test', async () => {
+    await Promise.all([
+      ProfileService.createProfile(normalData as ProfileCreateData),
+      ProfileService.createProfile(noWorkSpace as ProfileCreateData),
+    ]);
+    const result = await UserService.findAllProfile(1);
+    expect(result[0].hashTags[0].hashTag).toBe('중앙대학교');
+    expect(result[0].workSpace.hashTag).toBe('프렌딩');
+    expect(result[0].email).toBe('rlfehd2013@naver.com');
+  });
 });
