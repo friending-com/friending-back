6f94b66c3906912ecd01cb44da960951c5dcebdb
d0422 <rlfehd2013@naver.com>
2023-08-09 13:45:44 +0900

    feat: 이미지 업로드시 중복처리를 위한 코드 추가

--- src/services/ImageUploadService.ts
+++ src/services/ImageUploadService.ts
@@ -2,6 +2,7 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Request } from 'express';
 import multer from 'multer';
 import dotenv from 'dotenv';
+import ErrorStatus from '../utils/ErrorStatus';
 dotenv.config();
 const s3Config = {
   region: 'ap-northeast-2',
@@ -15,9 +16,11 @@ export const multerSetting = multer({ storage: storage });
 export class ImageUploadService {
   static client = new S3Client(s3Config);
   static async upload(req: Request) {
+    const [name, type] = getFileType(req.file.originalname);
+    const currnetTime = getCurrentDateTime();
     const file = {
       Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: req.file.originalname,
+      Key: currnetTime + name + type,
       Body: req.file.buffer,
     };
     try {
@@ -30,3 +33,34 @@ export class ImageUploadService {
     }
   }
 }
+
+const getFileType = (file: string) => {
+  if (!file.match(/.png|.jpeg|.jpg/)) {
+    throw new ErrorStatus(
+      'jpeg, jpg, png형식의 파일만 업로드 가능합니다.',
+      400
+    );
+  }
+  let type: string = '';
+  if (file.match(/.png/)) type = '.png';
+  if (file.match(/.jpeg/)) type = '.jpeg';
+  if (file.match(/.jpg/)) type = 'jpg';
+
+  const [name] = file.split(/.png|.jpeg|.jpg/);
+  return [name, type];
+};
+
+const getCurrentDateTime = () => {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  const formattedDateTime = `${year}-${month}-${day}.${hours}:${minutes}:${seconds}`;
+  return formattedDateTime;
+};
