78d84edee89b5def562c6939c42007ac4cafe4f6
d0422 <rlfehd2013@naver.com>
2023-08-19 17:29:12 +0900

    test: callbackController kakao테스트 추가

--- src/controllers/test/CallbackController.ts
+++ src/controllers/test/CallbackController.ts
@@ -4,7 +4,7 @@ import { LoginService } from '../../services/LoginService';
 import { CallbackController } from '../CallbackController';
 
 describe('callback Controller', () => {
-  it('', async () => {
+  it('google', async () => {
     const req: any = {
       body: {
         accessToken: 'googleAccessToken',
@@ -29,6 +29,44 @@ describe('callback Controller', () => {
     await CallbackController.post(req as Request, res as Response);
     expect(CallbackService.google).toHaveBeenCalledWith('googleAccessToken');
     expect(LoginService.login).toHaveBeenCalledWith(mockGoogleData);
+    expect(res.json).toHaveBeenCalledWith(mockTokenData);
+  });
+
+  it('kakao', async () => {
+    const req: any = {
+      body: {
+        accessToken: 'kakaoAccessToken',
+        platform: 'kakao',
+      },
+    };
+    const mockKakaoData = {
+      id: 'rlfehd2013',
+      kakao_account: {
+        profile: {
+          nickname: '동길',
+          profile_image_url: 'https://~',
+        },
+        name: '길동',
+        email: 'rlfehd2013@naver.com',
+        gender: 'male',
+        birthday: '0422',
+      },
+    };
+    const mockTokenData = {
+      access: 'asdf',
+      refresh: 'asdf',
+    };
+    const res: any = {
+      json: jest.fn(),
+    };
+    jest.spyOn(CallbackService, 'kakao').mockResolvedValue(mockKakaoData);
+    jest.spyOn(LoginService, 'login').mockResolvedValue(mockTokenData);
+
+    await CallbackController.post(req as Request, res as Response);
+    expect(CallbackService.kakao).toHaveBeenCalledWith('kakaoAccessToken');
+    expect(LoginService.login).toHaveBeenCalledWith(
+      mockKakaoData.kakao_account
+    );
     expect(res.json).toHaveBeenCalledWith(mockTokenData);
   });
 });
