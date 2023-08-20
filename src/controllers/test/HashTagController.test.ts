ee2702e944c05a83b7116a5e5a99eebbe80ab545
d0422 <rlfehd2013@naver.com>
2023-08-20 18:09:24 +0900

    test: HashTagController delete 테스트 추가

--- src/controllers/test/HashTagController.test.ts
+++ src/controllers/test/HashTagController.test.ts
@@ -46,4 +46,30 @@ describe('HashTagController', () => {
     expect(HashTagService.search).toHaveBeenCalledWith('해시태그');
     expect(res.json).toHaveBeenCalledWith([]);
   });
+
+  it('delete', async () => {
+    const req: any = {
+      headers: {
+        authorization: '',
+      },
+      query: {
+        hashTagName: '해시태그',
+        profileId: 2,
+      },
+    };
+    const res: any = {
+      json: jest.fn(),
+    };
+    jest.spyOn(JWTService, 'verify').mockResolvedValue({ id: 1 });
+    jest.spyOn(HashTagService, 'delete').mockResolvedValue(undefined);
+    jest
+      .spyOn(AuthorizationService, 'doesUserHaveProfile')
+      .mockResolvedValue({} as Profile);
+    await HashTagController.delete(req as Request, res as Response);
+
+    expect(JWTService.verify).toHaveBeenCalledWith('');
+    expect(AuthorizationService.doesUserHaveProfile).toHaveBeenCalledWith(1, 2);
+    expect(HashTagService.delete).toHaveBeenCalledWith('해시태그', 2);
+    expect(res.json).toHaveBeenCalledWith('hashTag가 삭제되었습니다');
+  });
 });
