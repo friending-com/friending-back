f4190574d335768f18441fe9170f4ce6b6d9b750
d0422 <rlfehd2013@naver.com>
2023-08-20 20:46:49 +0900

    test: ImageController delete 테스트 추가

--- src/controllers/test/ImageController.test.ts
+++ src/controllers/test/ImageController.test.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { ImageUploadService } from '../../services/ImageUploadService';
 import { ImageController } from '../ImageController';
+import { DeleteObjectCommandOutput } from '@aws-sdk/client-s3';
 
 describe('ImageController', () => {
   it('post', async () => {
@@ -15,5 +16,23 @@ describe('ImageController', () => {
     jest.spyOn(ImageUploadService, 'upload').mockResolvedValue('이미지링크URL');
     await ImageController.post(req as Request, res as Response);
     expect(res.json).toHaveBeenCalledWith('이미지링크URL');
+  });
+
+  it('delete', async () => {
+    const req: any = {
+      query: {
+        fileName: 'image.png',
+      },
+    };
+
+    const res: any = {
+      json: jest.fn(),
+    };
+    jest
+      .spyOn(ImageUploadService, 'delete')
+      .mockResolvedValue(true as unknown as DeleteObjectCommandOutput);
+    await ImageController.delete(req as Request, res as Response);
+    expect(ImageUploadService.delete).toHaveBeenCalledWith('image.png');
+    expect(res.json).toHaveBeenCalledWith(true);
   });
 });
