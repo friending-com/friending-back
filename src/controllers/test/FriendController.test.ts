5d67ce99ebedee1388ce05f4d2856a82b367fa66
d0422 <rlfehd2013@naver.com>
2023-08-20 15:55:03 +0900

    test: post 테스트 코드 작성

---
+++ src/controllers/test/FriendController.test.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { Profile } from '../../entity/Profile';
+import { AuthorizationService } from '../../services/AuthorizationService';
+import { JWTService } from '../../services/JWTService';
+import { FriendController } from '../FriendController';
+import { FriendService } from '../../services/FriendService';
+
+describe('friend Controller', () => {
+  it('post', async () => {
+    jest.spyOn(JWTService, 'verify').mockResolvedValue({ id: 1 });
+    jest
+      .spyOn(AuthorizationService, 'doesUserHaveProfile')
+      .mockResolvedValue(true as unknown as Profile);
+    jest.spyOn(FriendService, 'add').mockReturnValue(undefined);
+    const req: any = {
+      headers: {
+        authorization: '',
+      },
+      body: {
+        userProfileId: 1,
+        subProfileId: 2,
+      },
+    };
+    const res: any = {
+      json: jest.fn(),
+    };
+    await FriendController.post(req as Request, res as Response);
+    expect(AuthorizationService.doesUserHaveProfile).toHaveBeenCalledWith(1, 1);
+    expect(JWTService.verify).toHaveBeenCalledWith('');
+  });
+});
