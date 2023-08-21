9f3e88b5f93e936c056e063d25dad95e4e78ef8d
d0422 <rlfehd2013@naver.com>
2023-08-21 16:08:53 +0900

    test: ProfileController patch 테스트 추가

--- src/controllers/test/ProfileController.test.ts
+++ src/controllers/test/ProfileController.test.ts
@@ -84,4 +84,54 @@ describe('ProfileController', () => {
     expect(ProfileService.createProfile).toHaveBeenCalledWith(result);
     expect(res.json).toHaveBeenCalledWith('성공');
   });
+
+  it('patch', async () => {
+    const req: any = {
+      headers: {
+        authorization: '',
+      },
+      body: {
+        name: '길동',
+        isPublic: true,
+        hashTags: ['중앙대학교', '산업보안', '테스트1', '테스트2'],
+        email: 'rlfehd2013@naver.com',
+        workSpace: '프렌딩',
+      },
+      params: {
+        id: 2,
+      },
+    };
+
+    const result = {
+      id: 2,
+      discord: undefined,
+      email: 'rlfehd2013@naver.com',
+      facebook: undefined,
+      hashTags: ['중앙대학교', '산업보안', '테스트1', '테스트2'],
+      image: undefined,
+      instagram: undefined,
+      kakaoTalk: undefined,
+      line: undefined,
+      name: '길동',
+      naverBand: undefined,
+      naverBlog: undefined,
+      nickName: undefined,
+      phone: undefined,
+      telegram: undefined,
+      twitter: undefined,
+      userId: 3,
+      workSpace: '프렌딩',
+    };
+    const res: any = {
+      json: jest.fn(),
+    };
+    jest.spyOn(JWTService, 'verify').mockResolvedValue({ id: 3 });
+    jest
+      .spyOn(ProfileService, 'modifyProfile')
+      .mockResolvedValue({} as Profile);
+    await ProfileController.patch(req as Request, res as Response);
+    expect(JWTService.verify).toHaveBeenCalledWith('');
+    expect(ProfileService.modifyProfile).toHaveBeenCalledWith(result);
+    expect(res.json).toHaveBeenCalledWith({});
+  });
 });
