f0a9fcf7b4d2022bd6a586465e2b34c8e66b336a
d0422 <rlfehd2013@naver.com>
2023-08-21 13:46:24 +0900

    test: NavigateController test 추가

---
+++ src/controllers/test/NavigateController.test.ts
@@ -0,0 +1,36 @@
+import { JWTService } from '../../services/JWTService';
+import { AuthorizationService } from '../../services/AuthorizationService';
+import { Profile } from '../../entity/Profile';
+import { NavigateService } from '../../services/NavigateService';
+import { NavigateController } from '../NavigateController';
+import { Request, Response } from 'express';
+
+describe('NavigateController', () => {
+  it('post', async () => {
+    const req: any = {
+      headers: {
+        authorization: '',
+      },
+      body: {
+        findProfile: 2,
+        userProfile: 3,
+      },
+    };
+
+    const res: any = {
+      json: jest.fn(),
+    };
+
+    jest.spyOn(JWTService, 'verify').mockResolvedValue({ id: 1 });
+    jest
+      .spyOn(AuthorizationService, 'doesUserHaveProfile')
+      .mockResolvedValue({} as Profile);
+    jest.spyOn(NavigateService, 'navigate').mockResolvedValue([]);
+
+    await NavigateController.post(req as Request, res as Response);
+    expect(JWTService.verify).toHaveBeenCalledWith('');
+    expect(AuthorizationService.doesUserHaveProfile).toHaveBeenCalledWith(1, 3);
+    expect(NavigateService.navigate).toHaveBeenCalledWith(3, 2);
+    expect(res.json).toHaveBeenCalledWith([]);
+  });
+});
