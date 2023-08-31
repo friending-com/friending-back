f6a3c6e3f304be58b68332c47b8d2bf462dddab0
d0422 <rlfehd2013@naver.com>
2023-08-31 23:25:43 +0900

    feat: 카테고리 api서비스 로직 구성

---
+++ src/services/CategoryService.ts
@@ -0,0 +1,43 @@
+import UserDAO from '../DAO/UserDAO';
+import { AppDataSource } from '../DAO/data-source';
+import { Category } from '../entity/Category';
+import { Profile } from '../entity/Profile';
+import { User } from '../entity/User';
+
+export class CategoryService {
+  static repo = AppDataSource.getRepository(Category);
+
+  static async getAllCategory(userId: number) {
+    const user = await AppDataSource.getRepository(User).findOne({
+      where: { id: userId },
+    });
+    return await CategoryService.repo.findOne({ where: { user: user } });
+  }
+
+  static async getCategory(id: number) {
+    const category = await CategoryService.repo.findOne({
+      where: { id },
+      relations: {
+        friends: true,
+      },
+    });
+    const result = { name: category.name, friends: category.friends };
+    return result;
+  }
+
+  static async addFriendCategory(friendProfileId: number, id: number) {
+    const friendProfile = await AppDataSource.getRepository(Profile).findOne({
+      where: { id: friendProfileId },
+    });
+    const category = await CategoryService.repo.findOne({ where: { id } });
+    category.friends.push(friendProfile);
+    await CategoryService.repo.save(category);
+  }
+
+  static async makeCategory(userId: number, name: string) {
+    const category = new Category();
+    category.name = name;
+    category.user = await UserDAO.getUser(userId);
+    await CategoryService.repo.save(category);
+  }
+}
