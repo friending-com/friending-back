42eb0a612a04a573ab115b32f6e606fe64a85f99
d0422 <rlfehd2013@naver.com>
2023-09-16 16:43:29 +0900

    fix: route 잘못 등록 된 부분 수정

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -8,6 +8,6 @@ router.post('/', errorHandler(CategoryController.make));
 router.delete('/:categoryId', errorHandler(CategoryController.removeAll));
 
 router.post('/friend', errorHandler(CategoryController.add));
-router.delete('/friend', errorHandler(CategoryController.add));
+router.delete('/friend', errorHandler(CategoryController.remove));
 
 export { router as category };
