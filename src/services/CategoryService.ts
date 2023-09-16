b99c2e1873675a07361a53f60d45d7e9c26b8432
d0422 <rlfehd2013@naver.com>
2023-09-16 12:52:34 +0900

    feat: removeFriendCategory, removeCategory추가

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -3,6 +3,7 @@ import { AppDataSource } from '../DAO/data-source';
 import { Category } from '../entity/Category';
 import { Profile } from '../entity/Profile';
 import { User } from '../entity/User';
+import ErrorStatus from '../utils/ErrorStatus';
 
 export class CategoryService {
   static repo = AppDataSource.getRepository(Category);
@@ -34,10 +35,29 @@ export class CategoryService {
     await CategoryService.repo.save(category);
   }
 
+  static async removeFriendCategory(friendProfileId: number, id: number) {
+    const friendProfile = await AppDataSource.getRepository(Profile).findOne({
+      where: { id: friendProfileId },
+    });
+    const category = await CategoryService.repo.findOne({ where: { id } });
+    category.friends.filter((each) => each != friendProfile);
+    await CategoryService.repo.save(category);
+  }
+
   static async makeCategory(userId: number, name: string) {
     const category = new Category();
     category.name = name;
     category.user = await UserDAO.getUser(userId);
     return await CategoryService.repo.save(category);
+  }
+
+  static async removeCategory(userId: number, categoryId: number) {
+    const category = CategoryService.repo.findOne({
+      where: { user: await UserDAO.getUser(userId) },
+    });
+    if (category) {
+      return await CategoryService.repo.delete(categoryId);
+    }
+    throw new ErrorStatus('권한이 없습니다.', 401);
   }
 }
